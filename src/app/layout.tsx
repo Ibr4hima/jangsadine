4355868c52df9a0ac4318fa8e60dc3cc38adfd4b
Ibrahima Ba <ibrahima.28@outlook.com>
2026-04-21 16:28:44 -0400

    mise a jour

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'Jàng sa Diné — Apprends ta religion',
-  description: 'Cours audio islamiques, Quran, invocations et rappels en français.',
+  title: 'Jàng sa Diné',
+  //description: 'Jàng,  audio islamiques, Quran, invocations et rappels en français.',
   icons: { icon: '/logo.png' },
 }
 
